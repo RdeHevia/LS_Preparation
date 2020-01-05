3940d1d45ecd56062ff26d573623440e4e735b78
Raul de Hevia <raul.dehevia@gmail.com>
2020-01-05 09:37:27 -0800

    Version 1

---
+++ 1_IntroJS/10_Exercises/Arrays1.js
@@ -0,0 +1,5 @@
+planets=['Earth', 'Moon', 'Mars'];
+
+let first=arr=>arr[0];
+
+console.log(first(planets));
---
+++ 1_IntroJS/10_Exercises/Arrays2.js
@@ -0,0 +1,5 @@
+planets=['Earth', 'Moon', 'Mars'];
+
+let last=arr=>arr[arr.length-1];
+
+console.log(last(planets));
